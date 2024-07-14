2e7b25a75cca2b18cdeb4b5e0891f5740828da86
SeM2x <houssem135.302@gmail.com>
2024-07-14 23:29:28 +0100

    task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
+deletes out-of-date archives, using the function do_clean
 """
 from fabric.api import env, hide, local, run
 
 
-#env.hosts = ['35.174.207.53', '54.237.227.188']
-env.hosts = ['35.174.207.53']
+env.hosts = ['100.25.163.188', '34.203.77.186']
+
 
 def get_date(filename):
     return int(filename.split('.')[0].split('_')[-1])
@@ -14,13 +15,14 @@ def get_date(filename):
 def clean_local(number):
     with hide('commands'):
         res = local('ls versions', capture=True)
-    files = res.split('\n');
+    files = res.split('\n')
     dates = [get_date(filename) for filename in files]
     dates = sorted(dates, reverse=True)[number:]
 
     for filename in files:
         if get_date(filename) in dates:
             local(f'rm -f versions/{filename}')
+
 
 def clean_remote(number):
     path = '/data/web_static/releases/'
@@ -40,5 +42,5 @@ def do_clean(number=0):
     number = int(number)
     if number == 0:
         number = 1
-    #clean_local(number)
+    clean_local(number)
     clean_remote(number)
