1a133eb1cb337787d383a704d1c22aeef43f7d7f
SeM2x <houssem135.302@gmail.com>
2024-07-14 23:56:27 +0100

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,44 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""pack and deploy content to server
+"""
+from fabric.api import env, run, put, local
+import os
 
-env.hosts = ["100.25.163.188", "34.203.77.186"]
+env.hosts = ['100.25.163.188', '34.203.77.186']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+    """deploy package to remote server
+    Arguments:
+        archive_path: path to archive to deploy
     """
-    if os.path.isfile(archive_path) is False:
+    if not archive_path or not os.path.exists(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    put(archive_path, '/tmp')
+
+    file_name = archive_path.split('/')[-1]
+    dir_name = file_name.split('.')[0]
+
+    try:
+        run('mkdir -p /data/web_static/releases/{}/'.format(dir_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+                file_name, dir_name
+        ))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(
+                dir_name, dir_name
+        ))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            dir_name
+        ))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(
+            dir_name
+        ))
+        print("New version deployed!")
+        return True
+    except:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
