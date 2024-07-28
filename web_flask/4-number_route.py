528a2b7cabbea4711a7e7af73640416e7f37a205
SeM2x <houssm135.302@gmail.com>
2024-07-28 08:29:20 +0100

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,10 +28,9 @@ def py_is_cool(text='is cool'):
     return f"Python {text.replace('_', ' ')}"
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    if type(n) is int:
-        return f"{n} is a number"
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":
