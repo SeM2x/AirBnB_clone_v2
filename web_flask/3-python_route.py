8fdc5cd42d904ce413f65a34f501dad2212aeaa5
SeM2x <houssm135.302@gmail.com>
2024-07-28 08:23:49 +0100

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c_is_fun(text):
 
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def py_is_cool(text = 'is cool'):
+def py_is_cool(text='is cool'):
     return f"Python {text.replace('_', ' ')}"
 
 
