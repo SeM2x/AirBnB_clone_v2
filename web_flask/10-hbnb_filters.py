15a4d7790db47c78c1c03570720628559fe820d0
SeM2x <houssm135.302@gmail.com>
2024-07-28 23:42:50 +0100

    11. HBNB filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def filters():
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
